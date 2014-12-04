6b7704733d272a94c7741f200997d51831e0de52
Ray Braaf <braaf.raymond@gmail.com>
2014-12-03 21:29:37 -0500

    added jQuery to comments and answers

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,3 +1,14 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$( document ).ready(function() {
+  console.log('Locked and Loaded');
+  $('.question-comments-btn').click(function(){
+    $('.question-comment-container').slideToggle();
+  });
+
+  $('.new-question-comment').click(function(){
+    $('.question-comment-field').slideToggle();
+  });
+
+  $('.new-answer-btn').click(function(){
+    $('.answers-container').slideToggle();
+  });
+});
