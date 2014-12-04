fc7b136c3784ef6efdec686726f15d0b711712e5
Ray Braaf <braaf.raymond@gmail.com>
2014-12-04 14:06:39 -0500

    Added jQuery fix, prevent default

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,14 +1,17 @@
-$( document ).ready(function() {
+$( document ).on('ready page:load', function() {
   console.log('Locked and Loaded');
-  $('.question-comments-btn').click(function(){
+  $('.question-comments-btn').click(function(event){
+    event.preventDefault();
     $('.question-comment-container').slideToggle();
   });
 
-  $('.new-question-comment').click(function(){
+  $('.new-question-comment').click(function(event){
+    event.preventDefault();
     $('.question-comment-field').slideToggle();
   });
 
-  $('.new-answer-btn').click(function(){
+  $('.new-answer-btn').click(function(event){
+    event.preventDefault();
     $('.answers-container').slideToggle();
   });
 });
