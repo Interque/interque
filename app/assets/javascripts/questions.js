36c58755e35303d0636aaa9ad7dbf63096fe67ab
Ray Braaf <braaf.raymond@gmail.com>
2014-12-04 14:46:32 -0500

    cleaned up JS and CSS files, removed extra code

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,15 +1,5 @@
 $( document ).on('ready page:load', function() {
   console.log('Locked and Loaded');
-  $('.question-comments-btn').click(function(event){
-    event.preventDefault();
-    $('.question-comment-container').slideToggle();
-  });
-
-  $('.new-question-comment').click(function(event){
-    event.preventDefault();
-    $('.question-comment-field').slideToggle();
-  });
-
   $('.new-answer-btn').click(function(event){
     event.preventDefault();
     $('.answers-container').slideToggle();
