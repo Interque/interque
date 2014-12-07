a1b6ed8b4cf5b88218cb0c42a03ee7678d037ae4
Todd Metheny <todd.metheny@gmail.com>
2014-12-07 18:41:22 -0500

    wrestled with modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def new_question
-  	@question = Question.new
-  end
 
 private
 
