50c652155e586784aa04cc70250127a0afa279bf
Ray Braaf <braaf.raymond@gmail.com>
2014-12-13 23:43:44 -0500

    major style changes

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,7 +1,7 @@
 class SplashController < ApplicationController
-	def index
-		if current_user
-			redirect_to questions_path
-		end
-	end
+  def index
+    if current_user
+      redirect_to questions_path
+    end
+  end
 end
