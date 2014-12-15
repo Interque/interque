c58a703b3e6239702ee9d49e0be7214485239976
Todd Metheny <todd.metheny@gmail.com>
2014-12-15 15:46:39 -0500

    removed show from required_login before action

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 RSpec.describe Question, :type => :model do
   it "should create a question" do
 		a = Question.count
-		u = Question.create(title: "Question", description: "How's it going?", user_id: 1, tag_list: "yes")
+		#u = User.create(name: "Jim-Bob")
+		q = Question.create(title: "Question", description: "How's it going?", user_id: 1, tag_list: "yes")
 		# expect(Question.count).to be(1)
-		# expect(Question.first).to eq(u)
+		# expect(Question.first).to eq(q)
 		expect(Question.count > a).to be_truthy
 	end
 end
