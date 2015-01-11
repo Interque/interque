8b84ac58b4e68a3145f5910c2ea4596b78250e5e
Todd Metheny <todd.metheny@gmail.com>
2015-01-10 23:18:15 -0500

    playing around with the admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,9 @@
 class AdminController < ApplicationController
   def index
   	@questions = Question.all
-    
+
   	unless current_user && current_user.administrator?
 			redirect_to root_path
 		end
   end
 end
-
-# private 
-
-  # def activate
-  # 	@question = Question.find(params[:id])
-  # 	@question.approved = true
-  # 	@question.save
-  # end
