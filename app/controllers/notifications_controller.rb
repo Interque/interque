5ac3b50225881f87735550c9276b180b342ab533
Todd Metheny <todd.metheny@gmail.com>
2015-01-23 15:10:49 -0500

    fixed notifications for upvoting and downvoting

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,7 @@ class NotificationsController < ApplicationController
   def index
   	@questions = Question.all.order(:updated_at => :desc)
   	@users = User.all
+  	@answers = Answer.all.order(:updated_at => :desc)
+  	# @question = Question.find(params[:id])
   end
 end
