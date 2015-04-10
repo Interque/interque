0441f46409f2f7c43323725ee5e1457957608c7c
Todd Metheny <todd.metheny@kipusystems.com>
2015-04-10 12:18:18 -0400

    tracking number of answers given to a users questions

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,13 +4,22 @@ class NotificationsController < ApplicationController
   	@users = User.all
   	@answers = Answer.all.order(:updated_at => :desc)
   	@total_votes = 0
+  	@total_user_question_answers = 0
   	num_votes
+  	total_answers_to_user_questions
   end
 
   def num_votes
   	current_user.answers.find_each do |answer|
-  		@total_votes += answer.votes_for.size
+  		@total_votes += answer.votes_for.count
   	end
   	@total_votes
+  end
+
+  def total_answers_to_user_questions
+  	current_user.questions.find_each do |question|
+  		@total_user_question_answers += question.answers.count
+  	end
+  	@total_user_question_answers
   end
 end
