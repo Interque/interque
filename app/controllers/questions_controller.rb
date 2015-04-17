0aac6e5862d09f48a3c7eff6cf01a3e3fc6a3490
Todd Metheny <todd.metheny@gmail.com>
2015-04-16 22:26:28 -0400

    removed expire_fragment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -123,7 +123,7 @@ class QuestionsController < ApplicationController
       notification = ReadNotification.find_by(:user_id => current_user.id)
       notification.read_at = nil
       notification.save
-      expire_fragment("questions")
+      # expire_fragment("questions")
     else
       render @question
     end
