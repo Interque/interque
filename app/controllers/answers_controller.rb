f10a9074b469602d6e0d3de3ef2a88d5608b4b8b
Todd Metheny <todd.metheny@kipusystems.com>
2015-04-22 16:36:33 -0400

    trying to fix notifications

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -36,9 +36,9 @@ class AnswersController < ApplicationController
         format.html { redirect_to :back, notice: 'Answer was successfully created.' }
         format.json { render :show, status: :created, location: @answer }
         # expire_fragment("approvals")
-        notification = ReadNotification.find_by(:user_id => @answer.question.user_id)
-        notification.read_at = nil
-        notification.save
+        # notification = ReadNotification.find_by(:user_id => @answer.question.user_id)
+        # notification.read_at = nil
+        # notification.save
       else
         format.html { render :new }
         format.json { render json: @answer.errors, status: :unprocessable_entity }
@@ -100,6 +100,12 @@ class AnswersController < ApplicationController
       format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def notify_unread
+    notification = ReadNotification.find_by(:user_id => @answer.question.user_id)
+    notification.read_at = nil
+    notification.save 
   end
 
   # def total_votes
