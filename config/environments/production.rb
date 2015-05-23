90e5ec86e6b70022148c57c311fd6eea681f4a92
Todd Metheny <todd.metheny@gmail.com>
2015-05-23 17:58:23 -0400

    sender address for exception notification needs to be valid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.middleware.use ExceptionNotification::Rack,
     :email => {
-      sender_address: 'noreply@interque.co',
+      sender_address: 'team@interque.co',
       exception_recipients: ['todd@interque.co', 'ray@interque.co']
       # ignore_exceptions: ExceptionNotifier.default_ignore_exceptions # + [RuntimeError]
       
