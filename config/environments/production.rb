fb9578d230e2161b0b68a14046672153949e53b8
Todd Metheny <todd.metheny@gmail.com>
2015-05-23 17:53:18 -0400

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  Change mail delvery to either :smtp, :sendmail, :file, :test
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
